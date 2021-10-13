f28b8d04d2ae25eb04accc0f4dbadae063aeea66
alterperson <vad.mk@ya.ru>
2021-10-13 20:09:40 +0300

    fix: fixed goods block

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 const menuBtn = document.querySelector(".js-menu");
 const menu = document.querySelector(".menu");
+const loves = document.querySelectorAll(".icon__path");
 
 menuBtn.addEventListener("click", () => menu.classList.toggle("menu--mobile"));
+
+loves.forEach((love) => {
+  love.addEventListener("click", () => love.classList.toggle("icon__path--active"));
+});
