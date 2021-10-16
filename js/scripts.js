e652ad740ebea9ec03a6d8f73f0d6ee36bb54920
alterperson <vad.mk@ya.ru>
2021-10-16 13:07:38 +0300

    create: stories section & responsive & js-video

--- js/scripts.js
+++ js/scripts.js
@@ -1,104 +1,177 @@
-const menuBtn = document.querySelector(".js-menu");
-const menu = document.querySelector(".menu");
-const loves = document.querySelectorAll(".icon__path");
+document.addEventListener("DOMContentLoaded", function () {
+  const menuBtn = document.querySelector(".js-menu");
+  const menu = document.querySelector(".menu");
+  const loves = document.querySelectorAll(".icon__path");
+  const bookmarks = document.querySelectorAll(".stories-item__bookmark");
+  const playBtnMain = document.querySelector(".stories-item__button--main");
+  const playBtnFirst = document.querySelector(".stories-item__button--first");
+  const playBtnSecond = document.querySelector(".stories-item__button--second");
+  const playBtnThird = document.querySelector(".stories-item__button--third");
 
-menuBtn.addEventListener("click", () => menu.classList.toggle("menu--mobile"));
+  var player;
 
-loves.forEach((love) => {
-  love.addEventListener("click", () => love.classList.toggle("icon__path--active"));
+  menuBtn.addEventListener("click", () => menu.classList.toggle("menu--mobile"));
+
+  loves.forEach((love) => {
+    love.addEventListener("click", () => love.classList.toggle("icon__path--active"));
+  });
+
+  bookmarks.forEach((bookmark) => {
+    bookmark.addEventListener("click", () => bookmark.classList.toggle("stories-item__bookmark--active"));
+  });
+
+  const advantagesSwiper = new Swiper(".advantages-swiper", {
+    watchOverflow: false,
+    slidesPerView: 4,
+    spaceBetween: 26,
+
+    breakpoints: {
+      577: {
+        slidesPerView: 2,
+      },
+      769: {
+        slidesPerView: 2,
+      },
+      993: {
+        slidesPerView: 3,
+      },
+      1201: {
+        slidesPerView: 4,
+      },
+    },
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+    navigation: {
+      prevEl: ".advantages-slider__button--prev",
+      nextEl: ".advantages-slider__button--next",
+    },
+  });
+
+  const advantagesSwiperGrid = new Swiper(".advantages-swiper-grid", {
+    watchOverflow: false,
+    slidesPerView: 2,
+    grid: {
+      rows: 2,
+    },
+    spaceBetween: 10,
+
+    navigation: {
+      prevEl: ".advantages-slider-grid__button--prev",
+      nextEl: ".advantages-slider-grid__button--next",
+    },
+  });
+
+  const unpublisedSwiper = new Swiper(".unpublished-swiper", {
+    watchOverflow: false,
+    slidesPerView: 1,
+    spaceBetween: 30,
+
+    breakpoints: {
+      577: {
+        slidesPerView: 2,
+      },
+      769: {
+        slidesPerView: 3,
+      },
+      993: {
+        slidesPerView: 4,
+      },
+      1201: {
+        slidesPerView: 5,
+      },
+    },
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+    navigation: {
+      nextEl: ".unpublished-slider__button--next",
+      prevEl: ".unpublished-slider__button--prev",
+    },
+  });
+
+  playBtnMain.addEventListener("click", function onYouTubeIframeAPIReady() {
+    player = new YT.Player("player", {
+      height: "383",
+      width: "100%",
+      videoId: "iik25wqIuFo",
+      events: {
+        onReady: videoPlay,
+      },
+    });
+  });
+
+  function videoPlay(event) {
+    player.target.videoPlay();
+  }
+
+  playBtnFirst.addEventListener("click", function onYouTubeIframeAPIReady() {
+    player = new YT.Player("player1", {
+      height: "383",
+      width: "100%",
+      videoId: "iik25wqIuFo",
+      events: {
+        onReady: videoPlay,
+      },
+    });
+  });
+
+  function videoPlay(event) {
+    player1.target.videoPlay();
+  }
+
+  playBtnSecond.addEventListener("click", function onYouTubeIframeAPIReady() {
+    player = new YT.Player("player2", {
+      height: "383",
+      width: "100%",
+      videoId: "iik25wqIuFo",
+      events: {
+        onReady: videoPlay,
+      },
+    });
+  });
+
+  function videoPlay(event) {
+    player2.target.videoPlay();
+  }
+
+  playBtnThird.addEventListener("click", function onYouTubeIframeAPIReady() {
+    player = new YT.Player("player3", {
+      height: "383",
+      width: "100%",
+      videoId: "iik25wqIuFo",
+      events: {
+        onReady: videoPlay,
+      },
+    });
+  });
+
+  function videoPlay(event) {
+    player3.target.videoPlay();
+  }
+
+  // $(".advantages-slider").slick({
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 3,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 645,
+  //       settings: {
+  //         slidesToShow: 2,
+  //       },
+  //     },
+  //   ],
+  // });
 });
-
-const advantagesSwiper = new Swiper(".advantages-swiper", {
-  watchOverflow: false,
-  slidesPerView: 4,
-  spaceBetween: 26,
-
-  breakpoints: {
-    577: {
-      slidesPerView: 2,
-    },
-    769: {
-      slidesPerView: 2,
-    },
-    993: {
-      slidesPerView: 3,
-    },
-    1201: {
-      slidesPerView: 4,
-    },
-  },
-
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-
-  navigation: {
-    prevEl: ".advantages-slider__button--prev",
-    nextEl: ".advantages-slider__button--next",
-  },
-});
-
-const advantagesSwiperGrid = new Swiper(".advantages-swiper-grid", {
-  watchOverflow: false,
-  slidesPerView: 2,
-  grid: {
-    rows: 2,
-  },
-  spaceBetween: 10,
-
-  navigation: {
-    prevEl: ".advantages-slider-grid__button--prev",
-    nextEl: ".advantages-slider-grid__button--next",
-  },
-});
-
-const unpublisedSwiper = new Swiper(".unpublished-swiper", {
-  watchOverflow: false,
-  slidesPerView: 1,
-  spaceBetween: 30,
-
-  breakpoints: {
-    577: {
-      slidesPerView: 2,
-    },
-    769: {
-      slidesPerView: 3,
-    },
-    993: {
-      slidesPerView: 4,
-    },
-    1201: {
-      slidesPerView: 5,
-    },
-  },
-
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-
-  navigation: {
-    nextEl: ".unpublished-slider__button--next",
-    prevEl: ".unpublished-slider__button--prev",
-  },
-});
-
-// $(".advantages-slider").slick({
-//   slidesToShow: 4,
-//   slidesToScroll: 1,
-//   responsive: [
-//     {
-//       breakpoint: 1024,
-//       settings: {
-//         slidesToShow: 3,
-//       },
-//     },
-//     {
-//       breakpoint: 645,
-//       settings: {
-//         slidesToShow: 2,
-//       },
-//     },
-//   ],
-// });
